8a86d2c600426c9cfb80abf7c94b8744fcaaa154
Michael Torres <torresmi@mail.gvsu.edu>
2018-08-05 10:30:26 -0400

    add generator extensions

--- test-util/src/main/kotlin/com/fuzzyfunctors/reductions/testutil/Generators.kt
+++ test-util/src/main/kotlin/com/fuzzyfunctors/reductions/testutil/Generators.kt
@@ -1,8 +1,12 @@
 package com.fuzzyfunctors.reductions.testutil
 
 import com.fuzzyfunctors.reductions.core.alert.Alert
+import com.fuzzyfunctors.reductions.core.deal.Deal
+import com.fuzzyfunctors.reductions.core.game.Game
+import com.fuzzyfunctors.reductions.core.game.GameBestDeal
 import com.fuzzyfunctors.reductions.core.store.Store
 import io.kotlintest.properties.Gen
+import java.util.Date
 
 fun <A> Gen<A>.firstRandom(): A = random().first()
 
@@ -12,6 +16,16 @@ fun randomBool(): Boolean = Gen.bool().firstRandom()
 
 fun randomInt(): Int = Gen.int().firstRandom()
 
+fun randomLong(): Long = Gen.long().firstRandom()
+
 fun randomStore(): Store = StoreGenerator().firstRandom()
 
 fun randomAlert(): Alert = AlertGenerator().firstRandom()
+
+fun randomDeal(): Deal = DealGenerator().firstRandom()
+
+fun randomGame(): Game = GameGenerator().firstRandom()
+
+fun randomGameBestDeal(): GameBestDeal = GameBestDealGenerator().firstRandom()
+
+fun randomDate(): Date = Date(randomLong())
