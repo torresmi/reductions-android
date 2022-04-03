27797a5974b5d5aefe9a7066c187cabd8358f9d6
Michael Torres <torresmi@mail.gvsu.edu>
2022-04-02 23:41:34 -0600

    formatting

--- app/src/debug/kotlin/com/fuzzyfunctors/reductions/flipper/FlipperInitialization.kt
+++ app/src/debug/kotlin/com/fuzzyfunctors/reductions/flipper/FlipperInitialization.kt
@@ -6,7 +6,6 @@ import com.facebook.flipper.plugins.inspector.DescriptorMapping
 import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
 import com.facebook.soloader.SoLoader
 
-
 fun Application.initializeFlipper() {
     SoLoader.init(this, false)
 
