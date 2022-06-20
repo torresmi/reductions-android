e4e0100845c6366d79340c882d0b92ae1603812d
Michael Torres <torresmi@mail.gvsu.edu>
2022-06-20 11:15:36 -0600

    move sonarqube logic

--- gradle/reductions-build-logic/build.gradle.kts
+++ gradle/reductions-build-logic/build.gradle.kts
@@ -8,4 +8,5 @@ dependencies {
     implementation(libs.detekt.gradle.plugin)
     implementation(libs.kotlin.gradle.plugin)
     implementation(libs.semver.gradle.plugin)
+    implementation(libs.sonarqube.gradle.plugin)
 }
