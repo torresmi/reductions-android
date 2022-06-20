5cdaa387a5255b4fc4eca421abffad09ae68784b
Michael Torres <torresmi@mail.gvsu.edu>
2022-06-20 07:34:29 -0600

    move jacoco logic

--- gradle/reductions-build-logic/src/main/kotlin/android-application-convention.gradle.kts
+++ gradle/reductions-build-logic/src/main/kotlin/android-application-convention.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("android")
     id("com.android.application")
     id("detekt-convention")
+    id("jacoco-convention")
     id("kapt-convention")
     id("kotlin-convention")
     id("material3-convention")
