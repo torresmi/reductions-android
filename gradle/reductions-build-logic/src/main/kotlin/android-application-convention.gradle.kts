319799c24218436392252cfdf8d97d22081c756e
Michael Torres <torresmi@mail.gvsu.edu>
2022-06-20 00:45:53 -0600

    rerun detekt baselines

--- gradle/reductions-build-logic/src/main/kotlin/android-application-convention.gradle.kts
+++ gradle/reductions-build-logic/src/main/kotlin/android-application-convention.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("android")
     id("com.android.application")
+    id("detekt-convention")
     id("kapt-convention")
     id("kotlin-convention")
     id("material3-convention")
--- gradle/reductions-build-logic/src/main/kotlin/android-library-convention.gradle.kts
+++ gradle/reductions-build-logic/src/main/kotlin/android-library-convention.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("android")
     id("com.android.library")
+    id("detekt-convention")
     id("kapt-convention")
     id("kotlin-convention")
 }
