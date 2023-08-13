2fdd770281565087e64efdff508b5bd2c7b268d0
Michael Torres <mike@torres.dev>
2023-08-13 16:52:00 -0600

    setup stores modules

---
+++ feature/stores/stores-api/build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    id("kotlin-library-convention")
+}
+
+dependencies {
+    implementation(platform(libs.kotlin.bom))
+}
---
+++ feature/stores/stores-domain/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("kotlin-library-convention")
+}
+
+dependencies {
+    implementation(platform(libs.kotlin.bom))
+
+    implementation(libs.remotedata)
+
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.mockk)
+    testImplementation(project(":test-util"))
+}
