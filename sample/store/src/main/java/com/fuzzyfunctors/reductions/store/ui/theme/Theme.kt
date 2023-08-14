6daca944926598e52e0f69f5404e774c6c0861a8
Michael Torres <mike@torres.dev>
2023-08-13 23:37:55 -0600

    format

--- sample/store/src/main/java/com/fuzzyfunctors/reductions/store/ui/theme/Theme.kt
+++ sample/store/src/main/java/com/fuzzyfunctors/reductions/store/ui/theme/Theme.kt
@@ -1,25 +1,17 @@
 package com.fuzzyfunctors.reductions.store.ui.theme
 
 import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
-
 
 @Composable
 fun ReductionsandroidTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit,
 ) {
     val view = LocalView.current
     if (!view.isInEditMode) {
@@ -30,6 +22,6 @@ fun ReductionsandroidTheme(
     }
 
     MaterialTheme(
-        content = content
+        content = content,
     )
 }
