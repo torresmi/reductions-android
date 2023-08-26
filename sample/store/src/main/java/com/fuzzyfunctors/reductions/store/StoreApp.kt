b08d6c8c68c0a238b621236ff1bdd57adec7792b
Michael Torres <mike@torres.dev>
2023-08-26 11:05:53 -0600

    Formatting

--- sample/store/src/main/java/com/fuzzyfunctors/reductions/store/StoreApp.kt
+++ sample/store/src/main/java/com/fuzzyfunctors/reductions/store/StoreApp.kt
@@ -19,7 +19,7 @@ class StoreApp : Application() {
             loadKoinModules(
                 listOf(
                     flipperModule,
-                )
+                ),
             )
         }
 
